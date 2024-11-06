75ae66a4a6cd3610a3073cc408e55c230d6554d2
Slavena Slavova <106436147+SlavenaS@users.noreply.github.com>
2024-11-06 10:30:13 +0200

    zadacha 2.2 dopulena
    dopulena sus open file i save file

--- zadacha2.2/zadacha2.2/Form1.Designer.cs
+++ zadacha2.2/zadacha2.2/Form1.Designer.cs
@@ -30,15 +30,20 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(258, 207);
+            this.button1.Location = new System.Drawing.Point(48, 83);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(160, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "smqna na fon";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -46,17 +51,53 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 240);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "tursene na dok";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "suhranqvane na tekst";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(596, 294);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,6 +105,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- zadacha2.2/zadacha2.2/Form1.cs
+++ zadacha2.2/zadacha2.2/Form1.cs
@@ -15,21 +15,48 @@ namespace zadacha2._2
         public Form1()
         {
             InitializeComponent();
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+            openFileDialog2.Filter = "Text files (*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files(*.*)|*.*";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
                     this.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
                 }
-                catch 
+                catch
                 {
                     MessageBox.Show("Greshka");
                 }
             }
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            //izbira i poluchava izbraniq fail
+            string filename = openFileDialog2.FileName;
+            //prochita faila v tekstovata kutiq
+            string filetext = System.IO.File.ReadAllText(filename);
+            textBox1.Text = filetext;
+            MessageBox.Show("File is found");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            //izbira i poluchava izbraniq fail
+            string filename = saveFileDialog1.FileName;
+            //prochita faila v tekstovata kutiq
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("File is saved");
         }
     }
 }
